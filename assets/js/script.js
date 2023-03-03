bcff2bfadb7ab3f56944a41ba3fae9e08bb60c68
JanicaCosta <joaocarocco@gmail.com>
2023-03-03 15:05:02 +0000

    after a meeting with my mentor, has been done some adjustments on html/css and java, to improve functions and give me guidance how to complete the project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,109 @@
-/** starting script project */
+const cells = document.querySelectorAll(".cell");
+let checkTurn = true;
 
-const cells = document.querySelectorAll(".cell")
 
-let checkTurn = true ;
+const combinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-/**defining players */
-
+/** defining players */
 let playerX = "X";
 let playerO = "O";
-
-/** winner combinations */
-
-let winnerCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-/**defining event when clicking in the space to show the 2 players*/
-
-document.addEventListener("click", myfunction);
-
-function myfunction(event) {
-    if(event.target.matches(".cell")){
-        startClicking(event.target.id)
-    }
-}; 
-
-/** function beelow is to play/starting the game when we click in the cell */
-
-function startClicking(id) {
-    let cell = document.getElementById(id);
-    turn = checkTurn ? playerX : playerO;
-    cell.textContent = turn;
-    checkTurn = !checkTurn;
-    cell.classList.add(turn);
-    checkWinner(turn);
-};
-
-/** function below will be to check the winner using the combinations above */
-
-function checkWinner(turn){
-    const winner = winnerCombinations.some((comb) => {
-        return comb.every((index) => {
-          return cells[index].classList.contains(turn);
-        });
-      });
-
-      if (winner) {
-        gameCompleted(turn);
-      } else if (checkDraw()) {
-        gameCompleted();
-      } else {
-        checkTurn = !checkTurn;
-      }
+let scores = {
+  "X": 0,
+  "O": 0,
 }
 
-/**function below is to check if there is any drawn */
+cells.forEach(function (cell) {
+  cell.addEventListener("click", function cellClickCheck(event) {
+    cellClickedCallback(this);
+  })
+})
 
-function checkDraw(){
-    let x = 0;
-    let o = 0;
-    for (index in cells) {
-      if (!isNaN(index)) {
-        if (cells[index].classList.contains(playerX)) {
-          x++;
-        }
+/** defining event when clicking in the space to show either X or O */
+// document.addEventListener("click", function cellClickCheck(event){
+//         if(event.target.matches(".cell")){
+//             cellClickedCallback(event.target.id);
+//     }
+// });
+
+function cellClickedCallback(cell) {
+  turn = checkTurn ? playerX : playerO; // 'X' or 'O'
+  cell.textContent = turn;
+  checkTurn = !checkTurn;
+  // 'X' or 'O' are being added to check the winner not for styling purposes
+  cell.classList.add(turn);
+  checkWinner(turn);
+}
+
+function checkWinner(turn) {
+  const winner = combinations.some((comb) => {
+    return comb.every((index) => {
+      return cells[index].classList.contains(turn);
+    });
+  });
+
+  if (winner) {
+    gameCompleted(turn);
+  } else if (checkDraw()) {
+    gameCompleted();
+  }
+}
+
+
+function checkDraw(turn) {
+  let x = 0;
+  let o = 0;
+  for (index in cells) {
+    if (!isNaN(index)) {
+      if (cells[index].classList.contains(playerX)) {
+        x++;
+      }
+
+      if (cells[index].classList.contains(playerO)) {
+        o++;
+      }
+    }
+  }
+  return x + o === 9 ? true : false;
+}
+
+function gameCompleted(winner) {
+  endOfGame = true;
+  const result = document.getElementById("result");
   
-        if (cells[index].classList.contains(playerO)) {
-          o++;
-        }
-      }
-    }
-    return x + o === 9 ? true : false;
+
+  if (winner) {
+    scores[winner] += 1;;
+    
+  } else {
+    scores[playerO] += 1;;
+
+  } 
+
+  // let timer = 20;
+  // setInterval(() => {
+  //   h3.innerHTML = "Starting a new game in " + timer-- + " seconds";
+  // }, 1000);
+
+  // setTimeout(() => location.reload(), 20000);
+  updateScores();
+  clearCells();
+}
+ 
+
+function clearCells() {
+  
 }
 
-function gameCompleted()
+function updateScores(){
+  document.getElementById("win").innerHTML = scores[playerX]
+  document.getElementById("loose").innerHTML = scores[playerO]
+}
